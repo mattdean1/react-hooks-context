b128d8d0abf18bf0ad158fdcc7e98585aba50ffe
mattdean1 <mail@mattdean.io>
2020-06-07 12:59:31 +0100

    Add tests for selectors

--- client/src/store/selectors.js
+++ client/src/store/selectors.js
@@ -1,9 +1,3 @@
-export const getTotalPrice = (basket, productsMap) =>
-  Object.entries(basket).reduce((acc, curr) => {
-    const [productId, freq] = curr
-    return acc + productsMap[productId].price * freq
-  }, 0)
-
 export const getNumItems = (basket) =>
   Object.values(basket).reduce((acc, curr) => {
     return acc + curr
@@ -11,11 +5,16 @@ export const getNumItems = (basket) =>
 
 export const isProductRemovable = (basket, id) => basket[id] > 0
 
+export const getTotalPrice = (basket, productsMap) =>
+  Object.entries(basket).reduce((acc, curr) => {
+    const [productId, freq] = curr
+    return acc + productsMap[productId].price * freq
+  }, 0)
+
 export const getValidBasketItems = (basket, productsMap) =>
   Object.entries(basket)
-    .filter(([productId, freq]) => freq > 0)
-    .reduce((acc, curr) => {
-      const [productId, freq] = curr
-      acc.push({...productsMap[productId], freq } )
+    .filter(([_, freq]) => freq > 0)
+    .reduce((acc, [productId, freq]) => {
+      acc.push({ ...productsMap[productId], freq })
       return acc
     }, [])
--- client/src/store/selectors.spec.js
+++ client/src/store/selectors.spec.js
@@ -1,3 +1,43 @@
+import { getNumItems, isProductRemovable, getTotalPrice } from './selectors'
+
 describe('selectors', () => {
-  describe('a')
+  describe('getNumItems', () => {
+    it('should return number of items', () => {
+      const basket = { 123: 1, 456: 2 }
+      expect(getNumItems(basket)).toEqual(3)
+    })
+
+    it('should return zero for an empty basket', () => {
+      const basket = {}
+      expect(getNumItems(basket)).toEqual(0)
+    })
+  })
+
+  describe('isProductRemovable', () => {
+    it('should return true for a product with quantity larger than zero', () => {
+      const basket = { 123: 2 }
+      expect(isProductRemovable(basket, 123)).toEqual(true)
+    })
+    it('should return false for a product with quantity of zero', () => {
+      const basket = { 123: 0 }
+      expect(isProductRemovable(basket, 123)).toEqual(false)
+    })
+    it('should return false for a product which is not in the basket', () => {
+      const basket = {}
+      expect(isProductRemovable(basket, 123)).toEqual(false)
+    })
+  })
+
+  describe('getTotalPrice', () => {
+    it('should compute the total price of the basket', () => {
+      const basket = { 123: 1, 456: 2 }
+      const productsMap = { 123: { price: 6 }, 456: { price: 7 } }
+      expect(getTotalPrice(basket, productsMap)).toEqual(20)
+    })
+    it('should return zero for an empty basket', () => {
+      const basket = {}
+      const productsMap = { 123: { price: 6 }, 456: { price: 7 } }
+      expect(getTotalPrice(basket, productsMap)).toEqual(0)
+    })
+  })
 })
